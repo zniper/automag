bd809aea2655289e1c34629378af2fd26a60f61c
Zniper <me.zniper@gmail.com>
2014-06-29 19:29:58 +0700

    Ensure slug is smaller than 50 chars and unique

--- magazine/apps/content/models.py
+++ magazine/apps/content/models.py
@@ -3,14 +3,17 @@ from datetime import datetime
 from django.db import models
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
+from django.conf import settings
 
 from articles.models import Article as CoreArticle
 from articles.models import Attachment
 
 
+DEFAULT_DB = getattr(settings, 'ARTICLES_DEFAULT_DB', 'default')
+
+
 class Category(models.Model):
     name = models.CharField(max_length=128)
-    slug = models.CharField(max_length=128)
     description = models.TextField(blank=True)
 
     class Meta:
@@ -23,8 +26,35 @@ class Category(models.Model):
         qset = Article.objects.filter(categories=self, status__is_live=True)
         return qset.order_by('-publish_date')[:number]
 
+    def get_unique_slug(self, slug, using=DEFAULT_DB):
+        """Iterates until a unique slug is found"""
+
+        # we need a publish date before we can do anything meaningful
+        if type(self.publish_date) is not datetime:
+            return slug
+
+        orig_slug = slug
+        year = self.publish_date.year
+        counter = 1
+
+        while True:
+            if len(slug) > 50:
+                slug = slug[:48]
+
+            not_unique = Article.objects.all()
+            if hasattr(not_unique, 'using'):
+                not_unique = not_unique.using(using)
+            not_unique = not_unique.filter(publish_date__year=year, slug=slug)
+
+            if len(not_unique) == 0:
+                return slug
+
+            slug = '%s-%s' % (orig_slug, counter)
+            counter += 1
+
 
 class Article(CoreArticle):
+    slug = models.SlugField(max_length=255, unique_for_year='publish_date')
     categories = models.ManyToManyField('Category')
     featured = models.BooleanField(default=False)
 
